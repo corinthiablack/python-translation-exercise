cae3772a72497ab17b48f88b07bdc679b2a39061
Corinthia R. Black <coriblack@auburn.edu>
2019-02-27 09:04:01 -0600

    added translate_sequence code
    down to 10 failures, but now 10 errors too :(:(:(

--- translate.py
+++ translate.py
@@ -13,6 +13,13 @@ def translate_sequence(rna_sequence, genetic_code):
     If `rna_sequence` is less than 3 bases long, or starts with a stop codon,
     an empty string is returned.
     """
+    if rna_sequence >=3 and not ("UGA", "UAA", "UGA"):
+        for rna_sequence in genetic_code:
+            print(value)
+
+    else:
+        return ''
+
     pass
 
 def get_all_translations(rna_sequence, genetic_code):
